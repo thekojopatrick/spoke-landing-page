a1f097d9aef882ce86933c9960e04acff4e58b23
Kojo Patrick <patrickasare84@gmail.com>
2021-03-29 22:31:52 +0000

    add hero ui in progress

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,9 +1,11 @@
+import Hero from './Hero/Hero';
 import { Navbar } from './Navbar/Navbar';
 
 export const Header = () => {
 	return (
 		<header>
 			<Navbar />
+			<Hero/>
 		</header>
 	);
 };
---
+++ src/sections/Header/Hero/Hero.tsx
@@ -0,0 +1,30 @@
+import classes from './Hero.module.css';
+import HeroImage from '../../../assets/Media/illustrations/HeroImage.svg';
+
+
+
+export default function Hero() {
+	return (
+		<div className={classes.HeroContainer}>
+			<div className={[classes.Flex, classes.ItemsCenter].join(' ')}>
+				<div className={classes.LeftHeroBox}>
+					<h1 className={classes.HeroText}>
+						<span className={classes.Highlight}>Summarize</span> all your <br />
+						<span className={classes.Highlight}>video-conversations</span>
+					</h1>
+					<p className={classes.HeroParagraph}>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+					</p>
+					<button className={classes.CTAButton}>Start to summarize now</button>
+				</div>
+
+				<div className={classes.RightHeroBox}>
+					<img className={classes.HeroImage} src={HeroImage} alt='' />
+				</div>
+			</div>
+		</div>
+	);
+}
