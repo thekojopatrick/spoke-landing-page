027dec1bb760c8958b64636a770a565f633cce1e
Kojo Patrick <patrickasare84@gmail.com>
2021-03-30 13:16:25 +0000

    add bookdemo in progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './styles/App.css';
 import Layout from './components/Layout/Layout';
 import { Header } from './sections/Header/Header';
 import Footer from './sections/Footer/Footer';
+import BookDemo from './sections/BookDemo/BookDemo';
 
 function App() {
   return (
     <div className="App">
     <Layout>
       <Header/>
+      <BookDemo/>
       <Footer/>
     </Layout>
     </div>
