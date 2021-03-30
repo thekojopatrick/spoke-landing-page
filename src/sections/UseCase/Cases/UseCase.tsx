00db9247817fceba96fe6e8220aeea7baa00b12d
Kojo Patrick <patrickasare84@gmail.com>
2021-03-30 23:31:24 +0000

    add usecase ui in progress

--- src/sections/UseCase/Cases/UseCase.tsx
+++ src/sections/UseCase/Cases/UseCase.tsx
@@ -1,68 +1,42 @@
 import CSS from 'csstype';
+import { ReactNode } from 'react';
+import classes from './UseCase.module.css';
 
-const GreenButton: CSS.Properties = {
-	width: '441px',
-	height: '64px',
-	background: ' #07DCA9',
-	color: '#fff',
-	borderRadius: '8px',
-	fontSize: '20px',
-	lineHeight: '32px',
-};
-
-const ColorGreen: CSS.Properties = {
+const ButtonColor: CSS.Properties = {
 	backgroundColor: ' #07DCA9',
 };
 
-export default function UseCase() {
+interface IProps {
+	children: ReactNode;
+	title: string;
+	buttonName:string;
+}
+
+export default function UseCase({ title, children,buttonName }: IProps) {
 	return (
-		<div className='lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0'>
-			<div className='flex flex-wrap -m-4'>
-				<div className=''>
-					<h1 className='font-bold title-font text-gray-900 mb-4 text-4xl text-center sm:text-left'>
-						UX Research / Qualitative research
-					</h1>
-					<ul className='list-none list-inside flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5'>
-						<li className='mb-4 inline-flex items-center justify-center text-black text-base'>
-							<span style={ColorGreen} className='w-4 h-4 mr-3 '></span>
-							<p>
-								Gain time: directly mark moments of interest
-								<span className='italic'>
-									<br />
-									(80% of the information in a video-conference does NOT matter)
-								</span>
-							</p>
-						</li>
-						<li className='mb-4  inline-flex items-center justify-center text-black text-base'>
-							<span style={ColorGreen} className='w-4 h-4 mr-3 '></span>
-							Perfect transcription
-						</li>
-						<li className='mb-4 inline-flex items-center justify-center text-black text-base'>
-							<span style={ColorGreen} className='w-4 h-4 mr-3'></span>
-							Never forget anything
-						</li>
-					</ul>
-					<button
-						style={GreenButton}
-						className='inline-flex items-center justify-center mt-16'
-					>
-						Summarize your UX & Qualitative Research
-						<span>
-							<svg
-								width='12'
-								height='12'
-								viewBox='0 0 12 12'
-								fill='none'
-								className='ml-4'
-							>
-								<path
-									d='M5.99719 11.3L5.13719 10.38L9.15719 6.64H0.397188V5.28H9.15719L5.13719 1.54L5.99719 0.619999L11.4372 5.92V6L5.99719 11.3Z'
-									fill='white'
-								/>
-							</svg>
-						</span>
-					</button>
-				</div>
+		<div className={classes.Container}>
+			<div>
+				<h1 className={classes.Title}>{title}</h1>
+				<ul className={[classes.FeatureLists, classes.Spaces].join(' ')}>
+					{children}
+				</ul>
+				<button style={ButtonColor} className={classes.PrimaryButton}>
+					{buttonName}
+					<span>
+						<svg
+							width='12'
+							height='12'
+							viewBox='0 0 12 12'
+							fill='none'
+							className={classes.MarginLeft}
+						>
+							<path
+								d='M5.99719 11.3L5.13719 10.38L9.15719 6.64H0.397188V5.28H9.15719L5.13719 1.54L5.99719 0.619999L11.4372 5.92V6L5.99719 11.3Z'
+								fill='white'
+							/>
+						</svg>
+					</span>
+				</button>
 			</div>
 		</div>
 	);
--- src/sections/UseCase/UseCases.tsx
+++ src/sections/UseCase/UseCases.tsx
@@ -1,5 +1,7 @@
 import UseCase from './Cases/UseCase';
 import Testimonial from './Testimonials/Testimonial';
+import CSS from 'csstype';
+import classes from './UseCases.module.css';
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import Person1 from '../../assets/Media/images/person1.png';
@@ -8,6 +10,13 @@ import Person2 from '../../assets/Media/images/person2.png';
 import illustration2 from '../../assets/Media/illustrations/RedCard.svg';
 import Person3 from '../../assets/Media/images/person3.png';
 import illustration3 from '../../assets/Media/illustrations/PurpleCard.svg';
+
+const SquareIcon: CSS.Properties = {
+	width: '8px',
+	height: '8px',
+	backgroundColor: '#07DCA9',
+	marginRight: '4px',
+};
 
 export default function UseCases() {
 	return (
@@ -19,7 +28,29 @@ export default function UseCases() {
 						personName={'John Doe ~ Big Corp'}
 						illustration={illustration1}
 					/>
-					<UseCase />
+					<UseCase
+						title={'UX Research / Qualitative research'}
+						buttonName={'Summarize your UX & Qualitative Research'}
+					>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							<p>
+								Gain time: directly mark moments of interest
+								<span className={classes.Italic}>
+									<br />
+									(80% of the information in a video-conference does NOT matter)
+								</span>
+							</p>
+						</li>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							Perfect transcription
+						</li>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							Never forget anything
+						</li>
+					</UseCase>
 				</div>
 			</div>
 			<div className='container px-5 py-24 mx-auto'>
@@ -29,7 +60,23 @@ export default function UseCases() {
 						personName={'John Doe ~ Big Corp'}
 						illustration={illustration2}
 					/>
-					<UseCase />
+					<UseCase
+						title={'Customer Success'}
+						buttonName={'Summarize your Customer Success'}
+					>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							<p>Playing around with your information</p>
+						</li>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							Create fun videos
+						</li>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							Remix your video-conferences
+						</li>
+					</UseCase>
 				</div>
 			</div>
 			<div className='container px-5 py-24 mx-auto'>
@@ -39,7 +86,29 @@ export default function UseCases() {
 						personName={'John Doe ~ Big Corp'}
 						illustration={illustration3}
 					/>
-					<UseCase />
+					<UseCase
+						title={'Internal Communication'}
+						buttonName={'Summarize your Internal Communication'}
+					>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							<p>Stop repeating yourself : archive and save it for ever.</p>
+						</li>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							Collectif : You’re gonna lose time by editing your videos, but
+							everyone in your company will benefit.
+						</li>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							Cut the bullshit : 80% of the information in a video-conference
+							does NOT matter
+						</li>
+						<li className={classes.Feature}>
+							<span style={SquareIcon}></span>
+							Knowledge: Personal library of all your video-conversations.
+						</li>
+					</UseCase>
 				</div>
 			</div>
 		</section>
