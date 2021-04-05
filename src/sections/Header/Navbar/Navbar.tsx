3e292eb4d0c9eb7c2a762f91c5f69df17a19e1d5
Kojo Patrick <patrickasare84@gmail.com>
2021-04-05 21:35:56 +0000

    add hamburger menu

--- src/sections/Header/Navbar/Navbar.tsx
+++ src/sections/Header/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react';
 
 //Importing classes
 import classes from './Navbar.module.css';
@@ -6,68 +6,95 @@ import classes from './Navbar.module.css';
 //Importing others
 import Logo from '../../../assets/landing/svg/logo/spoke.svg';
 
+class Navbar extends Component {
+	state = { clicked: false };
 
-function Navbar() {
-    return (
-        <nav className={classes.Nav}>
-            <div className={classes.Navbar}>
-                <div>
-                    <a href='/' className='pr-2 lg:pr-8 lg:px-6 focus:outline-none'>
-                        <img src={Logo} alt='logo' className={classes.Logo} />
-                    </a>
-                </div>
+	handleClick = () => {
+		this.setState({ clicked: !this.state.clicked });
+	};
+	render() {
+		return (
+			<nav className={classes.Nav}>
+				<div className={classes.Navbar}>
+					<div>
+						<a href='/' className='pr-2 lg:pr-8 lg:px-6 focus:outline-none'>
+							<img src={Logo} alt='logo' className={classes.Logo} />
+						</a>
+					</div>
 
-                {/* <!-- Mobile menu button --> */}
-                <div className={classes.HamburgerMenu}>
-                    <button
-                        type='button'
-                        className={classes.HamburgerMenuButton}
-                        aria-label='toggle menu'
-                    >
-                        <svg
-                            width='24'
-                            height='24'
-                            viewBox='0 0 24 24'
-                            fill='none'
-                            xmlns='http://www.w3.org/2000/svg'
-                        >
-                            <path
-                                d='M21 19H3V17H21V19ZM21 15H3V13H21V15ZM21 11H3V9H21V11ZM21 7H3V5H21V7Z'
-                                fill='#2E3A59'
-                            ></path>
-                        </svg>
-                    </button>
-                </div>
-                {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
-                <div className={classes.NavLinkGroup}>
-                    <div className={classes.NavLinks}>
-                        <a href='#how-it-works' className={classes.NavLink}>
-                            How it works
-						</a>
-                        <a href='#our-clients' className={classes.NavLink}>
-                            Our Clients
-						</a>
-                        <a href='#use-cases' className={classes.NavLink}>
-                            Use cases
-						</a>
-                        <a href='#why-spoke' className={classes.NavLink}>
-                            Why Spoke
-						</a>
-                        <a href='#privacy' className={classes.NavLink}>
-                            Privacy
-						</a>
-                        <a href='#contact-us' className={classes.NavLink}>
-                            Contact
-						</a>
-                    </div>
-                    <div className={classes.NavButtonsGroup}>
-                        <a className={classes.LoginButton}>Login</a>
-                        <a className={classes.RegisterButton}>Create an account</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+					{/* <!-- Mobile menu button --> */}
+					<div className={classes.HamburgerMenu} onClick={this.handleClick}>
+						<button
+							type='button'
+							className={classes.HamburgerMenuButton}
+							aria-label='toggle menu'
+						>
+							{this.state.clicked ? (
+								<svg
+									width='24'
+									height='24'
+									viewBox='0 0 24 24'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41L17.59 5Z'
+										fill='#2E3A59'
+									></path>
+								</svg>
+							) : (
+								<svg
+									width='24'
+									height='24'
+									viewBox='0 0 24 24'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M21 19H3V17H21V19ZM21 15H3V13H21V15ZM21 11H3V9H21V11ZM21 7H3V5H21V7Z'
+										fill='#2E3A59'
+									></path>
+								</svg>
+							)}
+						</button>
+					</div>
+					{/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+					<div
+						className={
+							this.state.clicked
+								? classes.NavLinkGroupActive
+								: classes.NavLinkGroup
+						}
+					>
+						<div className={classes.NavLinks}>
+							<a href='#how-it-works' className={classes.NavLink}>
+								How it works
+							</a>
+							<a href='#our-clients' className={classes.NavLink}>
+								Our Clients
+							</a>
+							<a href='#use-cases' className={classes.NavLink}>
+								Use cases
+							</a>
+							<a href='#why-spoke' className={classes.NavLink}>
+								Why Spoke
+							</a>
+							<a href='#privacy' className={classes.NavLink}>
+								Privacy
+							</a>
+							<a href='#contact-us' className={classes.NavLink}>
+								Contact
+							</a>
+						</div>
+						<div className={classes.NavButtonsGroup}>
+							<a className={classes.LoginButton}>Login</a>
+							<a className={classes.RegisterButton}>Create an account</a>
+						</div>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 }
 
-export default Navbar
+export default Navbar;
